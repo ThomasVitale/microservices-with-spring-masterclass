3e2610d441be0976b14d69fbc6286e00940c4aba
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-06-05 21:56:59 +0200

    Update lab 2

--- book-service/src/main/java/com/thomasvitale/bookservice/web/BookController.java
+++ book-service/src/main/java/com/thomasvitale/bookservice/web/BookController.java
@@ -6,7 +6,6 @@ import com.thomasvitale.bookservice.domain.BookRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
