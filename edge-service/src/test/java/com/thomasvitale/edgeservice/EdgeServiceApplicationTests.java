30d17ef1ab0ae66637cebe6c69e006911d3e6f25
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-06-06 00:50:37 +0200

    Add lab 5

---
+++ edge-service/src/test/java/com/thomasvitale/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.thomasvitale.edgeservice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
